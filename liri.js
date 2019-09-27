daa4a7b53c6c69df210bb47357e2d6e4b824cfea
Aaron Jackson <aarontjackson@outlook.com>
2019-09-27 16:29:30 -0400

    Bands In Town function is responding

--- liri.js
+++ liri.js
@@ -5,12 +5,14 @@ var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
+const chalk = require('chalk');
+
 
 var whatToDo = process.argv[2];
 var userInput = process.argv[3];
 
 
-const queryURL = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+
 
 function spotifyThis(input) {
     spotify
@@ -25,12 +27,31 @@ function spotifyThis(input) {
 
 async function concertThis(userInput) {
     try {
-        const response = await axios.get( "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp")
-            console.log(response);
+        const response = await axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp")
+        // console.log(response);
+        var jsondata = response.data;
+        console.log(jsondata);
+        
+
+        if (!jsondata.length) {
+            console.log("no results found for artist");
+            return;
+        }
+        for (let i = 0; i < jsondata.length; i++) {
+            let show = jsondata[i];
+            console.log(
+                
+                show.venue.city +
+                "," + 
+                (show.venue.region || show.venue.country) + " at " + show.venue.name + " " + moment(show.datetime).format("MM/DD/YYYY"))
+                
+         
+            };
     }
-    catch(error){
+    catch (error) {
         console.log(error);
     }
+
 }
 
 concertThis(userInput);
@@ -38,21 +59,21 @@ concertThis(userInput);
 async function movieThis(userInput) {
     try {
         const response = await axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy")
-            console.log(`Title: ${response.data.Title}`);
-            console.log(`Year: ${response.data.Year}`);
-            console.log(`IMBD Rating: ${response.data.Ratings[0].Value}`);
-            console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
-            console.log(`Country: ${response.data.Country}`);
-            console.log(`Language: ${response.data.Language}`);
-            console.log(`Plot: ${response.data.Plot}`);
-            console.log(`Actors: ${response.data.Actors}`);
+        console.log(`Title: ${response.data.Title}`);
+        console.log(`Year: ${response.data.Year}`);
+        console.log(`IMBD Rating: ${response.data.Ratings[0].Value}`);
+        console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
+        console.log(`Country: ${response.data.Country}`);
+        console.log(`Language: ${response.data.Language}`);
+        console.log(`Plot: ${response.data.Plot}`);
+        console.log(`Actors: ${response.data.Actors}`);
     }
-    catch (error){
+    catch (error) {
         console.log(error)
     }
 }
 
-movieThis(userInput);
+// movieThis(userInput);
 
 function doWhatItSays() {
 
@@ -62,12 +83,14 @@ switch (whatToDo) {
     case "spotify-this-song":
         spotifyThis(userInput);
         break;
-    case "movie-this":
-        movieThis(userInput);
-        break;
+
     case "concert-this":
         concertThis(userInput);
         break;
+    case "movie-this":
+        movieThis(userInput);
+        break;
+
     case "do-what-it-says":
         doWhatItSays();
         break;
